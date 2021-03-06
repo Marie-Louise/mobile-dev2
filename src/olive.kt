a09ea1b07c79fef411689d01e5441fdedcebba42
Marie-Louise <mariemuteteyimana@gmail.com>
2021-03-06 08:58:24 +0300

    first commit

---
+++ src/olive.kt
@@ -0,0 +1,31 @@
+fun main() {
+  name()
+   modulus()
+   sum()
+   personality()
+
+}
+ fun name() {
+     println("hello marie")
+
+ }
+fun modulus(){
+    var num1=20
+    var num2=7
+    var modulus=num1%num2
+    println(modulus)
+}
+fun sum(){
+    var num1=8
+    var num2=6
+    var num3=5
+    var num4=8
+    var sum =num1+num2+num3+num4
+    println(sum)
+}
+fun personality(){
+    println("honest")
+}
+
+
+
